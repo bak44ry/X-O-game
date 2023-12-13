ec115c70bfa2de20e1d46cd884ed84f772d82bed
bak44ry <bak44ry@gmail.com>
2023-12-13 14:02:18 +0300

    first commit

---
+++ lib/theme/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color.fromARGB(255, 134, 2, 2);
+  static Color secondaryColor = Color.fromARGB(255, 211, 7, 7);
+  static Color accentColor = const Color.fromARGB(255, 232, 232, 233);
+}
